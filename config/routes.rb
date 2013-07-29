08a9d27ca2a308dfe8c49e1f43004c88f864c78e
Robin Tran <duytran_it@yahoo.com.vn>
2013-07-29 20:33:12 +0700

    fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Ownproject::Application.routes.draw do
   get "welcome/index"
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   root 'welcome#index'
 end
